5f2e36f835db3aac1e45c4aff16f214aa19858dc
michaelcariaso <mdcariaso@up.edu.ph>
2024-06-01 20:17:12 +0800

    feat: added user controller

---
+++ backend/controllers/user_controller.js
@@ -0,0 +1,35 @@
+import pool from "mysql_pool";
+
+//add user
+export async function addUser(req, res) {
+  const { username, user_password, display_name, age } = req.body;
+
+  try {
+    const [rows] = await pool.query(
+      `INSERT INTO user (username, user_password, display_name, age) VALUES (?, ?, ?, ?);
+        `,
+      [username, user_password, display_name, age]
+    );
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to insert user to the database" });
+  }
+}
+
+//search user
+export async function searchUser(req, res) {
+  const username = req.query.username;
+  const user_password = req.query.password;
+  try {
+    const [rows] = await pool.query(
+      `SELECT * FROM user WHERE username = ? AND user_password = ?;
+          `,
+      [username, user_password]
+    );
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch user from the database" });
+  }
+}
--- backend/routes.js
+++ backend/routes.js
@@ -11,11 +11,14 @@ import {
   searchFoodItem,
 } from "./controllers/reports_controller.js";
 
+import { addUser, searchUser } from "./controllers/user_controller.js";
+
 const setUpRoutes = (app) => {
   app.get("/", (req, res) => {
     res.send("API Main Page");
   });
 
+  //reports
   app.get("/estabs", viewAllEstablishments);
   app.get("/estabs/food-review", viewAllFoodReviewsEstablishment);
   app.get("/food/food-review", viewAllFoodReviewsFoodItem);
@@ -26,6 +29,10 @@ const setUpRoutes = (app) => {
   app.get("/estabs/high-rating", viewAllHighAverageRatingEstablishments);
   app.get("/foods/order-price", viewAllFoodItemsOrderByPrice);
   app.get("/food", searchFoodItem);
+
+  //user
+  app.post("/user/add", addUser);
+  app.get("/user/search", searchUser);
 };
 
 export default setUpRoutes;
