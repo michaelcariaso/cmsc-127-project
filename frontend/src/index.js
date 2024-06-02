5dcebd80aa792bc59ceb30ae1bc4706acf968aed
michaelcariaso <mdcariaso@up.edu.ph>
2024-06-02 13:48:16 +0800

    review

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';  
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-import LogIn from './pages/log-in.js';
-import SignUp from './pages/sign-up.js';
+import LogIn from "./pages/log-in.js";
+import SignUp from "./pages/sign-up.js";
 
-import Establishments from './pages/view-establishments';
-import ReviewsEstab from './pages/estab-review.js';
-import EstabFood from './pages/view-food.js';
+import Establishments from "./pages/view-establishments";
+import ReviewsEstab from "./pages/estab-review.js";
+import EstabFood from "./pages/view-food.js";
+import ReviewsFood from "./pages/food-review.js";
 
 const router = createBrowserRouter([
   //LOGIN/SIGNUP
-  { path: '/', element: <LogIn/>},
-  { path: '/sign-up', element: <SignUp/>},
+  { path: "/", element: <LogIn /> },
+  { path: "/sign-up", element: <SignUp /> },
   //MENU
-  { path: '/estabs', element: <Establishments/>},
-  { path: '/estabs/food-review', element: <ReviewsEstab />},
-  { path: '/estabs/food', element: <EstabFood />}
+  { path: "/estabs", element: <Establishments /> },
+  { path: "/estabs/food-review", element: <ReviewsEstab /> },
+  { path: "/estabs/food", element: <EstabFood /> },
+  { path: "/estabs/food/food-review", element: <ReviewsFood /> },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
