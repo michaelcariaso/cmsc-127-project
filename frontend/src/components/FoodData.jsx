8df06e729ca9750959a0ad2d14170eacbbe25d9f
jobenoidz <jlaguirre@up.edu.ph>
2024-06-02 13:09:17 +0800

    feat: food items now fetchable

--- frontend/src/components/FoodData.jsx
+++ frontend/src/components/FoodData.jsx
@@ -3,16 +3,19 @@ import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import "../css/food.css";
 
 export default function FoodData({ data }) {
-  const [foodData, setFoodData] = useState(data || []);
-
+  
   useEffect(() => {
     console.log('Props Data:', data); // Log props data
     setFoodData(data);
   }, [data]);
+  
+  
+  const [foodData, setFoodData] = useState(data || []);
 
+  console.log(data);
   return (
     <div className="inventorytable-container">
-      <h1>ALL ESTABLISHMENTS</h1>
+      <h1>FOOD ITEMSS</h1>
       <div className="food-container">
         {foodData.map((food) => (
           <div className="food-group" key={food._id}>
@@ -20,12 +23,10 @@ export default function FoodData({ data }) {
               <img src="https://images.unsplash.com/photo-1590779033100-9f60a05a013d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt={food.name} />
             </div>
             <div className='food-deets'>
-              <h1 className="food-name">{food["food Name"]}</h1>
-              <p className='food-desc'>{food["Address"]}</p>
-              <p className='food-type'>CUISINE: {food["Cuisine"]}</p>
+              <h1 className="food-name">{food["item_name"]}</h1>
               <p className="food-price">AVG. RATING: {food["Average Rating"]}</p>
               <div className="estab-btn">
-                <Link to={`/estabs/food?establishment_id=${food.establishment_id}`}>
+                <Link to={`/estabs/food/food-review?establishment_id=${food.establishment_id}`}>
                   <button>SEE REVIEWS</button>
                 </Link>
               </div>
