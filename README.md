e5732333fce856bf8c9259503c461834dbd8c2cc
jobenoidz <jlaguirre@up.edu.ph>
2024-06-03 14:07:15 +0800

    Schema modified and readme added

---
+++ README.md
@@ -0,0 +1,20 @@
+# PROJECT FOR CMSC 127 - FOOD REVIEW APP
+2nd Sem A.Y. '23-'24
+
+### GROUP MEMBERS
+- Aguirre, John Benedict
+- Cariaso, Michael Uriel
+- Castro, Alexander John III
+
+## INSTRUCTIONS
+- Enter local sql credentials in `.env` file in /backend 
+- Run and initialize database in MariaDB
+-- `source <DIRECTORY>/cmsc-127-project/backend/schema.sql;` 
+- Install React and Express dependencies
+-- `npm i`
+- Run /backend
+-- `cd backend`
+-- `node server.js`
+- Run /backend
+-- `cd frontend`
+-- `npm start`
--- backend/schema.sql
+++ backend/schema.sql
@@ -5,49 +5,48 @@ USE cmsc_127_proj;
 
 --USER
 CREATE TABLE user(
-username VARCHAR(20) NOT NULL,
-user_password LONGTEXT NOT NULL,
-display_name VARCHAR(20) NOT NULL,
-age INT(2) NOT NULL,
-CONSTRAINT user_username_pk PRIMARY KEY (username)
+    username VARCHAR(20) NOT NULL,
+    user_password LONGTEXT NOT NULL,
+    display_name VARCHAR(20) NOT NULL,
+    age INT(2) NOT NULL,
+    CONSTRAINT user_username_pk PRIMARY KEY (username)
 );
 
 --FOOD_ESTABLISHMENT
 CREATE TABLE food_establishment(
-establishment_id VARCHAR(10) NOT NULL,
-establishment_name VARCHAR(20) NOT NULL,
-establishment_address VARCHAR(100) NOT NULL,
-establishment_cuisine VARCHAR(20) NOT NULL,
-CONSTRAINT food_establishment_pk PRIMARY KEY(establishment_id)
+    establishment_id VARCHAR(10) NOT NULL,
+    establishment_name VARCHAR(20) NOT NULL,
+    establishment_address VARCHAR(100) NOT NULL,
+    establishment_cuisine VARCHAR(20) NOT NULL,
+    CONSTRAINT food_establishment_pk PRIMARY KEY(establishment_id)
 );
 
 --FOOD_ITEM
 CREATE TABLE food_item(
-item_id VARCHAR(10) NOT NULL,
-item_name VARCHAR(20) NOT NULL,
-item_price decimal(5,2),
-food_type VARCHAR(5),
-establishment_id VARCHAR(10) NOT NULL,
-CONSTRAINT item_id_pk PRIMARY KEY(item_id),
-CONSTRAINT item_establishment_id FOREIGN KEY(establishment_id) REFERENCES
-food_establishment(establishment_id)
+    item_id VARCHAR(10) NOT NULL,
+    item_name VARCHAR(20) NOT NULL,
+    item_price decimal(5,2),
+    food_type VARCHAR(10),
+    establishment_id VARCHAR(10) NOT NULL,
+    CONSTRAINT item_id_pk PRIMARY KEY(item_id),
+    CONSTRAINT item_establishment_id FOREIGN KEY(establishment_id) REFERENCES
+    food_establishment(establishment_id)
 );
 
 --FOOD_REVIEW
 CREATE TABLE food_review(
-entry_id VARCHAR(20) NOT NULL,
-review VARCHAR(150),
-rating INT(1) NOT NULL,
-review_date date NOT NULL,
-review_time time NOT NULL,
-username VARCHAR(20) NOT NULL,
-establishment_id VARCHAR(20) NULL,
-item_id VARCHAR(20) NULL,
-CONSTRAINT food_review_pk PRIMARY KEY (entry_id),
-CONSTRAINT food_review_username FOREIGN KEY(username) REFERENCES user(username),
-FOREIGN KEY(establishment_id) REFERENCES
-food_establishment(establishment_id),
-FOREIGN KEY(item_id) REFERENCES food_item(item_id)
+    entry_id VARCHAR(20) NOT NULL,
+    review VARCHAR(150),
+    rating INT(1) NOT NULL,
+    review_date date NOT NULL,
+    review_time time NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    establishment_id VARCHAR(20) NULL,
+    item_id VARCHAR(20) NULL,
+    CONSTRAINT food_review_pk PRIMARY KEY (entry_id),
+    CONSTRAINT food_review_username FOREIGN KEY(username) REFERENCES user(username),
+    FOREIGN KEY(establishment_id) REFERENCES food_establishment(establishment_id),
+    FOREIGN KEY(item_id) REFERENCES food_item(item_id)
 );
 
 INSERT INTO user (username, user_password, display_name, age) VALUES ("mike123", "12345", "Mike", 35);
@@ -152,22 +151,22 @@ INSERT INTO food_item (item_id, item_name, item_price, food_type, establishment_
 VALUES ("jabee3", "Burger Steak", 20.00, "meat", "estab5");
 
 INSERT INTO food_item (item_id, item_name, item_price, food_type, establishment_id)
-VALUES ("jabee1", "ChickenJoy", 30.00, "meat", "estab6");
+VALUES ("jabee4", "ChickenJoy", 30.00, "meat", "estab6");
 
 INSERT INTO food_item (item_id, item_name, item_price, food_type, establishment_id)
-VALUES ("jabee4", "Peach Mango Pie", 10.00, "dessert", "estab6");
+VALUES ("jabee5", "Peach Mango Pie", 10.00, "dessert", "estab6");
 
 INSERT INTO food_item (item_id, item_name, item_price, food_type, establishment_id)
-VALUES ("jabee5", "JolliHotdog", 15.00, "meat", "estab6");
+VALUES ("jabee6", "JolliHotdog", 15.00, "meat", "estab6");
 
 INSERT INTO food_item (item_id, item_name, item_price, food_type, establishment_id)
-VALUES ("jabee1", "ChickenJoy", 30.00, "meat", "estab7");
+VALUES ("jabee7", "ChickenJoy", 30.00, "meat", "estab7");
 
 INSERT INTO food_item (item_id, item_name, item_price, food_type, establishment_id)
-VALUES ("jabee6", "JolliSpaghetti", 20.00, "pasta", "estab7");
+VALUES ("jabee8", "JolliSpaghetti", 20.00, "pasta", "estab7");
 
 INSERT INTO food_item (item_id, item_name, item_price, food_type, establishment_id)
-VALUES ("jabee7", "Chocolate Sundae", 10.00, "dessert", "estab7");
+VALUES ("jabee9", "Chocolate Sundae", 10.00, "dessert", "estab7");
 
 --FOOD REVIEWS
 --insert food review
